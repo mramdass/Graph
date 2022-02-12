071b304ba449387de14b0ad2996d3b6449af86c7
Munieshwar Ramdass <mramdass@live.com>
2022-02-12 13:12:02 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -14,6 +14,7 @@ import dash_html_components as html
 NODES = ['Ava', 'Bob', 'Chi', 'Dev', 'Eve']
 EDGES = [('Ava','Chi'), ('Bob','Dev'), ('Bob','Eve'), ('Chi', 'Eve')]
 
+# https://networkx.org/documentation/stable/tutorial.html
 G = nx.Graph()
 G.add_node('Ava')
 G.add_nodes_from(['Bob','Chi','Dev','Eve'])
